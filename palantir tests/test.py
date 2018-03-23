af4be7699b1bbb05e40d6b86ceeee34d39a9b46c
paalpri <primstadp@gmail.com>
2018-03-23 12:36:17 +1100

    all

---
+++ palantir tests/test.py
@@ -0,0 +1,24 @@
+
+import sys
+import os
+
+# Complete the function below.
+
+    
+
+def test(l, r):
+    odd = []
+    for i in range(l,r+1):
+        if (i % 2):
+           odd.append(i)
+
+    print (odd)
+
+
+
+l = 2
+r = 9
+test(2,9)
+    
+
+  
